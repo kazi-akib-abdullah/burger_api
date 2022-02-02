50a7fafdc510a7a3bb40e1df77e7cfec31e3495b
kazi-akib-abdullah <q.aqib.abdullah@gmail.com>
2022-02-03 03:38:16 +0600

    #Create - Initial Commit

---
+++ MyRestApi/burgerApi/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+# Create your views here.
+
+class UserProfileViewSet(ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
